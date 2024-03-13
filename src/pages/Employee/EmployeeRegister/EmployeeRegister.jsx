8f181c51c2e0f2d9ef710aa0e7f2dd6da20c4896
pnitijarasrat <puriwatds@gmail.com>
2024-03-13 20:16:20 +0700

    second commit

--- src/pages/Employee/EmployeeRegister/EmployeeRegister.jsx
+++ src/pages/Employee/EmployeeRegister/EmployeeRegister.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 import Card from '../../../components/Card/Card'
 import Header from '../../../components/Header/Header'
